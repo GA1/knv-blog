64c9411c4ba95f2e0f85f2d226f2424492bc4bea
Kamil NV <nvkga1@gmail.com>
2016-08-25 23:10:37 +0200

    Add single post browsing

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^posts/(?P<title>[\w ]+)', views.post),
+    url(r'^.*$', views.no_page_found),
+    #url(r'^/post/(?P<title>\w{0,50})$', views.post),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+import sys
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.all()
+    print('22222222222222222')
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post(request, title):
+    posts = Post.objects.filter(title=title)
+    print('11111111111111111')
+    print(title)
+    print('11111111111111111')
+    return render(request, 'blog/post_list.html', {'posts':posts})
+
+def no_page_found(request):
+    return render(request, 'blog/no_page_found.html',)
