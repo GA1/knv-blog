9cd27e8a455acc1c247f349f783f0f5aa72b8581
Kamil NV <nvkga1@gmail.com>
2016-09-03 12:25:31 +0200

    Sort post  desc by date

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 import sys
+import re
 
 def post_list(request):
-    posts = Post.objects.all().order_by('published_date')
+    posts = Post.objects.all().order_by('-published_date')
     for post in posts:
     	post.text = post.text[:100] + "......."
     return render(request, 'blog/post_list.html', {'posts':posts})
