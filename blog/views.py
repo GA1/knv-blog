92f255a505d0d1c45d6f0e2b140b6a113d501f7d
Kamil Nguyen Van <knguyen@zinio.com>
2016-09-21 19:04:27 +0200

    Show only non-draft posts

--- blog/views.py
+++ blog/views.py
@@ -6,31 +6,31 @@ import sys
 import re
 
 def post_list(request):
-    posts = Post.objects.all().order_by('-created_date')
-    for post in posts:
-    	post.text = post.text[:100] + "......."
-    return render(request, 'blog/post_list.html', {'posts':posts})
+  posts = Post.objects.all().filter(is_draft=False).order_by('-created_date')
+  for post in posts:
+    post.text = post.text[:100] + "......."
+  return render(request, 'blog/post_list.html', {'posts':posts})
 
 def post(request, title):
-    post = Post.objects.filter(title=title).first()
-    if post == None:
-        return no_page_found(request)
-    return render(request, 'blog/post.html', {'post':post})
+  post = Post.objects.filter(is_draft=False, title=title).first()
+  if post == None:
+    return no_page_found(request)
+  return render(request, 'blog/post.html', {'post':post})
 
 def writing_list(request):
-    writings = Writing.objects.all().order_by('-created_date')
-    for writing in writings:
-        writing.text = writing.text[:100] + "......."
-    return render(request, 'blog/writing_list.html', {'writings':writings})
+  writings = Writing.objects.all().order_by('-created_date')
+  for writing in writings:
+    writing.text = writing.text[:100] + "......."
+  return render(request, 'blog/writing_list.html', {'writings':writings})
 
 def writing(request, title):
-    writing = Writing.objects.filter(title=title).first()
-    if writing == None:
-        return no_page_found(request)
-    return render(request, 'blog/writing.html', {'writing':writing})
+  writing = Writing.objects.filter(title=title).first()
+  if writing == None:
+    return no_page_found(request)
+  return render(request, 'blog/writing.html', {'writing':writing})
 
 def no_page_found(request):
-    return render(request, 'blog/no_page_found.html', status=404)
+  return render(request, 'blog/no_page_found.html', status=404)
 
 
 
